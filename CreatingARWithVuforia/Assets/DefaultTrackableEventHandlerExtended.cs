ec77fb2341c6e77fd084437e3011175e261f315a
SirCalvino <and-gome@uniandes.edu.co>
2019-04-22 18:08:13 +0900

    Vuforia updated, added audio, instead of modifying script it was extended as it is recommended.

---
+++ CreatingARWithVuforia/Assets/DefaultTrackableEventHandlerExtended.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultTrackableEventHandlerExtended : DefaultTrackableEventHandler
+{
+    public GameObject audioPlayer;
+    private PlayOnStateChange player;
+
+    override protected void Start()
+    {
+        base.Start();
+        player = audioPlayer.GetComponent<PlayOnStateChange>();
+    }
+
+    //Extending the functionality when the target is found
+    override protected void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        if (player != null)
+        {
+            player.Initialize();
+            player.PlayOnAppear();
+        }
+    }
+
+    //Extending the functionality when the target dissapears
+    override protected void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+        if (player != null)
+        {
+            player.PlayOnDisappear();
+        }
+    }
+}
